fb2942083f1525e7f24cd10f60f36e36527b0d13
Cyphercomp <ndicunguyecedric@gmail.com>
2024-07-07 11:50:32 -0700

    changed case on Time Bound variable

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,23 +1,23 @@
 Task = input("Enter your Task:")
-Time_bound = input("is it Time_bound-bound? (yes/no)")
+Time_Bound = input("is it Time_Bound-bound? (yes/no)")
 Priority = input ("Priority (high/medium/low):")
 
 
 match Priority :
     case "high" :
-        if Time_bound == "yes" :
+        if Time_Bound == "yes" :
             print(f"Reminder: '{Task}' is a high Priority Task that requires immediate attention today!")
         else :
             print(f"Note: '{Task}' is a high Priority Task that requires immediate attention today!")   
     case "medium" :
-        if Time_bound == "yes" :
+        if Time_Bound == "yes" :
             print(f"Reminder: '{Task}' is a medium Priority Task that requires  attention today!")
         else :
             print(f"Note: '{Task}' is a medium Priority Task that requires  attention today!") 
     case "low" :
-        if Time_bound == "yes" :
-            print(f"Reminder: '{Task}' is a  low Priority Task. Consider completing it when you have free Time_bound.")
+        if Time_Bound == "yes" :
+            print(f"Reminder: '{Task}' is a  low Priority Task. Consider completing it when you have free Time_Bound.")
         else :
-            print(f"Note: '{Task}' is a low Priority Task. Consider completing it when you have free Time_bound.") 
+            print(f"Note: '{Task}' is a low Priority Task. Consider completing it when you have free Time_Bound.") 
     case _:
         print("Invalid day entered.")
