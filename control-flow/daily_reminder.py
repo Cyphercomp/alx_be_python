a804c33bf35d41f3db6408c11d50322c0709a41d
Cyphercomp <ndicunguyecedric@gmail.com>
2024-07-07 11:25:44 -0700

    Changed the order of the variable

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,22 +1,23 @@
 task = input("Enter your task:")
+time_bound = input("is it time_bound-bound? (yes/no)")
 priority = input ("Priority (high/medium/low):")
-time = input("is it time-bound? (yes/no)")
+
 
 match priority :
     case "high" :
-        if time == "yes" :
+        if time_bound == "yes" :
             print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
         else :
             print(f"Note: '{task}' is a high priority task that requires immediate attention today!")   
     case "medium" :
-        if time == "yes" :
+        if time_bound == "yes" :
             print(f"Reminder: '{task}' is a medium priority task that requires  attention today!")
         else :
             print(f"Note: '{task}' is a medium priority task that requires  attention today!") 
     case "low" :
-        if time == "yes" :
-            print(f"Reminder: '{task}' is a  low priority task. Consider completing it when you have free time.")
+        if time_bound == "yes" :
+            print(f"Reminder: '{task}' is a  low priority task. Consider completing it when you have free time_bound.")
         else :
-            print(f"Note: '{task}' is a low priority task. Consider completing it when you have free time.") 
+            print(f"Note: '{task}' is a low priority task. Consider completing it when you have free time_bound.") 
     case _:
         print("Invalid day entered.")
