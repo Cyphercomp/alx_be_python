e2ab1b3b942f82dd73e33f2feb3543a9aab5a061
Cyphercomp <ndicunguyecedric@gmail.com>
2024-07-13 10:48:08 -0700

    updated

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,19 +1,17 @@
-class BankAccount :
-     def __init__(self,account_balance=0) : 
-          self.account_balance = account_balance
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.account_balance = initial_balance
 
-     def deposit(self,amount) :
+    def deposit(self, amount):
         self.account_balance += amount
 
-     def withdraw(self, amount) : 
-         if amount <= self.account_balance :
+    def withdraw(self, amount):
+        if amount <= self.account_balance:
             self.account_balance -= amount
             return True
-         
-         else :
-          return False
+        else:
+            return False
 
-     def display_balance(self) :
-         print(f"Current Balance: ${self.account_balance:.2f}")
-
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance:.2f}")
 
